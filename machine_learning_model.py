f5c224d8d88daf0593eed61572dfbcedda4412df
HamzaA598 <hamzaemad598@gmail.com>
2023-12-04 19:53:07 +0200

    typo

--- machine_learning_model.py
+++ machine_learning_model.py
@@ -3,14 +3,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-NUMBER_OF_TEPETITIONS = 5
+NUMBER_OF_REPETITIONS = 5
 
 # data preprocessing
 X = []
 y = []
 
 # first experiment
-for i in range(NUMBER_OF_TEPETITIONS):
+for i in range(NUMBER_OF_REPETITIONS):
     # Split the data into training and testing sets randomly
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=None)
 
